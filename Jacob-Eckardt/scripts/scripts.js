01ef68c8a003e492c31d95849bebdfd678d47b1e
tannermc23 <tmcmillan@byupathway.edu>
2026-02-11 20:27:28 -0700

    Updated.

--- Jacob-Eckardt/scripts/scripts.js
+++ Jacob-Eckardt/scripts/scripts.js
@@ -5,13 +5,6 @@ yearSpan.textContent = now.getFullYear();
 const toggle = document.getElementById("themeToggle");
 const body = document.body;
 
-// Load saved preference
-const savedTheme = localStorage.getItem("theme");
-if (savedTheme === "dark") {
-    body.classList.add("dark-mode");
-    toggle.textContent = "☀️";
-}
-
 const hamburger = document.getElementById("hamburger");
 const nav = document.querySelector(".nav");
 
@@ -20,10 +13,3 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
 });
 
-toggle.addEventListener("click", () => {
-    body.classList.toggle("dark-mode");
-
-    const isDark = body.classList.contains("dark-mode");
-    toggle.textContent = isDark ? "☀️" : "🌙";
-    localStorage.setItem("theme", isDark ? "dark" : "light");
-});
